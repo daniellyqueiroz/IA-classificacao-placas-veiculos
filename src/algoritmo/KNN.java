e4320bcbe9fd30590663ebfc71faa33a64a33cc2
Tamires Pereira <tamirespereira6@gmail.com>
2017-08-02 22:42:21 -0300

    Ainda tá lendo errado

--- src/algoritmo/KNN.java
+++ src/algoritmo/KNN.java
@@ -1,12 +1,50 @@
 package algoritmo;
 
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class KNN{
 	
-	//Ler arquivo, colocar em lista
+	private static List<Imagem> LeArq(String arq) throws IOException {
+		List<Imagem> imagens = new ArrayList<Imagem>();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(arq));		
+		try {
+			String line = reader.readLine(); 
+						
+			while((line = reader.readLine()) != null && !(line.startsWith("%") && !(line.startsWith("@")))) {
+				String[] tokens = line.split(",");
+				int pos = line.indexOf(":");
+				Imagem imagem = new Imagem();
+				imagem.classe = Integer.parseInt(tokens[0]);
+				//System.out.println(imagem.classe);
+				imagem.dist = new double[tokens.length - 1];
+					for(int i = 1; i < tokens.length; i++) {
+						while(tokens[i] != line.substring(0,pos)){
+							imagem.dist[i-1] = Integer.parseInt(tokens[i]);	
+							//System.out.println(imagem.classe);
+						}
+						
+				}
+				
+						imagens.add(imagem);
+				
+			}	
+				
+		} finally {
+			reader.close();
+		}
+		
+		return imagens;
+	}
 	
-	public static double distEuclidiana(int[]x, int[]y){
+	public static double distEuclidiana(double x[], double y[]){
 		double soma = 0;
 		for(int i = 0; i < x.length; i++) {
 			soma += (x[i] - y[i]) * (x[i] - y[i]);
@@ -14,31 +52,15 @@ public class KNN{
 		return Math.sqrt(soma);
 	}
 	
-	public static double distHamming(int[]x, int[]y){
+	public static double distHamming(double[]x, double[]y){
 		double soma = 0;
 		for(int i = 0; i < x.length; i++) {
 			soma += (x[i] - y[i]);
 		}
 		return Math.abs(soma);
 	}
-	/* Não funciona
-	public static double pondera(int[]x,int[]y){
-		double w = 0;
-		for(int i = 0; i < x.length; i++){
-			w = 1 / (x[i] - y[i] * x[i] - y[i]);
-		}
-		return w;
-	}
 	
-	public static double distEuclidianaPonderada(int[]x,int[]y) {
-		double soma = 0;
-		for(int i = 0; i < x.length; i++) {
-			soma += (x[i] - y[i]) * (x[i] - y[i]) * pondera(x, y);
-		}
-		return Math.sqrt(soma);
-	}
-	*/
-	public static double distCosseno(int[]x,int[]y) { 
+	public static double distCosseno(double x[],double y[]) { 
 		double soma1 = 0;
 		double soma2 = 0;
 	 
@@ -50,30 +72,40 @@ public class KNN{
 	    
 	    return Math.abs(soma1/soma2);
 	  } 
-	/*Classificar menor distancia
-	 * 
-	 * 
-	public static int classificador(List<Imagem> imagens, int[] ent){
-		int leg = 0;
-		int melhorDist = Integer.MAX_VALUE;
-		int dist;
+	
+	public static double classificador(List<Imagem> imagens, double[] ent){
+		int classe = 0;
+		double melhorDist = Integer.MAX_VALUE;
+		double dist;
 		for(Imagem lista: imagens){
-			dist = distEuclidiana(lista.ent, ent);
+			dist = distEuclidiana(lista.dist, ent);
 			if(dist < melhorDist){
 				melhorDist = dist;
-				leg = lista.leg;
+				classe = lista.classe;				
 
 			}
 		}
-		return leg;
+		return melhorDist;
 	}
-	*/
-	public static void main(String[] argv)  {
-		int v[] = {2,1};
-		int s[] = {4,3};
-		for(int i = 0; i< v.length;i++){
-			System.out.println(distEuclidiana(v,s));
+	
+	static class Imagem{
+		int classe;
+		double dist[];
+	}
+		
+	
+	public static void main(String[] argv) throws IOException  {
+		List<Imagem> conjunto = LeArq("classificacaoDePlacas-0-455.arff");
+		
+		int classeCerta = 0;
+		
+	
+		for(Imagem imagens:conjunto) {
+			if(classificador(conjunto, imagens.dist) == imagens.classe)
+				classeCerta++;
+							
 		}
+		System.out.println(classeCerta);
 		
 	}
 }
