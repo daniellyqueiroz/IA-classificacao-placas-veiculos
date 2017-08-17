fd32e920ec636bf51925d2b7e01a7bfdda2eff0e
Tamires Pereira <tamirespereira6@gmail.com>
2017-08-16 21:38:30 -0300

    Testes

--- src/algoritmo/KNN.java
+++ src/algoritmo/KNN.java
@@ -5,19 +5,10 @@ package algoritmo;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
 
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 
 public class KNN{
@@ -37,16 +28,12 @@ public class KNN{
 							
 							Imagem imagem = new Imagem();
 							imagem.classe = Integer.parseInt(tokens[0]);
-							//Classes OK
-							//System.out.println(tokens[0]);
-											
+							
 							imagem.dist = new double[tokens.length - 1];
-							//Quantidade de atributos OK
-							//System.out.println(tokens.length);
-																			
+										
 							for(int i = 1; i < tokens.length; i++) {
 								imagem.dist[i-1] = Double.parseDouble(tokens[i]);
-								//System.out.println(tokens[i]);
+								
 							}
 							
 								imagens.add(imagem);
@@ -77,7 +64,7 @@ public class KNN{
 		double soma = 0;
 		for(int i = 0; i < x.length; i++) {
 			soma += Math.abs(x[i] - y[i]);
-			//System.out.println(x[i] + " - " + y[i]);
+			
 		}
 		return soma;
 	}
@@ -88,39 +75,14 @@ public class KNN{
 		double soma2 = 0;
 	 
 	    for (int i = 0; i < x.length; i++) {
-	    	/*
 	    		soma1 += (x[i] * y[i]);
 		    	soma2 += (Math.pow(x[i], 2) * Math.pow(y[i], 2));
-		    	//System.out.println(x[i] + " - " + y[i]);
-	    	*/
-	    	//soma1 = Math.max(Math.abs(x[i] - y[i]));
 	    } 
 	   
 	    
 	    return (soma1/soma2);
 	  } 
 	
-	public static int distanciaK1(List<Imagem> imagens, double[] ent){
-		int classe = 0;
-		double melhorDist = Integer.MAX_VALUE;
-		double dist = 0;
-		try{
-			for(Imagem lista: imagens){
-				dist = distEuclidiana(lista.dist, ent);
-				System.out.println(dist + " # Classe: " + lista.classe);//distancias certas
-				if(dist < melhorDist){
-					melhorDist = dist;
-					classe = lista.classe;
-					//System.out.println(classe + " dist: " + lista.dist);
-
-				}
-			}
-		}catch (IndexOutOfBoundsException e) {
-	           return 0;
-	       }
-		
-		return classe;
-	}
 	
 	public static int distancias(List<Imagem> imagens, double[] ent, int k) throws IOException{
 		double dist = 0;
@@ -132,14 +94,12 @@ public class KNN{
 		
 		for(Imagem lista: imagens){
 			Imagem2 im = new Imagem2();
-			dist = distManhattan(lista.dist, ent);	
+			dist = distCosseno(lista.dist, ent);	
 			im.setDist(dist);
 			im.setClasse(lista.classe);
 			knn.add(im);
 			Collections.sort(knn);
 			
-	
-			//System.err.println(knn);
 			
 	}	
 	knn = knn.subList(0, k);
@@ -151,7 +111,6 @@ public class KNN{
 		}
 	
 	}
-	//System.err.println(knn);
 	if(cont1 > cont2)
 		return 1;
 	else 		
@@ -161,18 +120,20 @@ public class KNN{
 	
 	
 	public static void classificacao() throws IOException{
-		List<Imagem> conjunto = LeArq("classificacaoDePlacas-0-455.arff");
-		List<Imagem> conjunto2 = LeArq("classificacaoDePlacas-0-455-2.arff");
+		List<Imagem> Treinamento = LeArq("classificacaoDePlacas-Treinamento.arff");
+		List<Imagem> Teste = LeArq("classificacaoDePlacas-Teste.arff");
 		
 		int qtdClasseCerta = 0;		
-		for(Imagem imagens:conjunto) {
-				if(distancias(conjunto, imagens.dist,1) == imagens.classe){
+		for(Imagem imagens:Treinamento) {
+				if(distancias(Teste, imagens.dist,15) == imagens.classe){
 					qtdClasseCerta++;
 				}									
 							
 		}
-		
-		System.out.println("Porcentagem classes certas: " + qtdClasseCerta);
+		System.err.println("Usando k = 15 com Distancia Cosseno: \n");
+	
+		System.out.println("Quantidade classes certas: " + qtdClasseCerta);
+		System.out.println("Porcentagem de acerto: " + (double)qtdClasseCerta / Treinamento.size() * 100 + "%");
 		
 	}
 	
@@ -218,6 +179,7 @@ public class KNN{
 		
 	
 	public static void main(String[] argv) throws IOException{
+		
 		classificacao();		
 		
 	}
